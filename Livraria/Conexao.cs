4762e523bef0b41c5461d5feb57cdf2a18dc8899
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-05-03 20:35:00 -0300

    Add Conexao class and tests

---
+++ Livraria/Conexao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria
+{
+    public static class Conexao
+    {
+        private static string strConexao = @"Data Source=localhost\sqlexpress;Initial Catalog=Livraria;User Id=sa;Password=feevale";
+        private static readonly SqlConnection instance = new SqlConnection(strConexao);
+
+        /// <summary>
+        /// Retorna um objeto do tipo SqlConnection contendo a conexão com o banco de dados
+        /// </summary>
+        public static SqlConnection Instance
+        {
+            get
+            {
+                if (instance.State == ConnectionState.Closed)
+                {
+                    instance.Open();
+                }
+
+                return instance;
+            }
+        }
+
+    }
+}
---
+++ LivrariaTest/ConexaoTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Livraria;
+
+namespace LivrariaTest
+{
+    [TestClass]
+    public class ConexaoTest
+    {
+        SqlConnection conexao = Conexao.Instance;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            conexao.ChangeDatabase("LivrariaTest");
+        }
+
+        [TestMethod]
+        public void TestInstanceState()
+        {
+            Assert.AreEqual(conexao.State, ConnectionState.Open);
+        }
+    }
+}
