fa2754cb07cb110910378f76cd9aaac7539d4eff
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-05-03 14:55:24 -0300

    Add test for autor properties

--- LivrariaTest/AutorTest.cs
+++ LivrariaTest/AutorTest.cs
@@ -1,15 +1,27 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Livraria;
 
 namespace LivrariaTest
 {
     [TestClass]
     public class AutorTest
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestProperties()
         {
-            Assert.AreEqual(true, true);
+            Autor autor = new Autor();
+
+            autor.CodAutor = 999;
+            autor.Nome = "George R. R. Martin";
+            autor.Cpf = "012.345.678.90";
+            autor.DtNascimento = new DateTime(1948, 9, 20);
+
+            Assert.AreEqual(autor.CodAutor, 999);
+            Assert.AreEqual(autor.Nome, "George R. R. Martin");
+            Assert.AreEqual(autor.Cpf, "012.345.678.90");
+            Assert.AreEqual<DateTime>(autor.DtNascimento, new DateTime(1948, 9, 20));
         }
 
     }
