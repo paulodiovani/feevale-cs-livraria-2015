7b878acc5d66660fcdd66e62972cedb21f5510e4
Paulo Diovani Gonçalves <paulo@diovani.com>
2015-05-03 18:58:37 -0300

    Add test for ListaAutores method

--- LivrariaTest/AutorTest.cs
+++ LivrariaTest/AutorTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Livraria;
+using System.Collections.Generic;
 
 namespace LivrariaTest
 {
@@ -22,6 +23,18 @@ namespace LivrariaTest
             Assert.AreEqual(autor.Nome, "George R. R. Martin");
             Assert.AreEqual(autor.Cpf, "012.345.678.90");
             Assert.AreEqual<DateTime>(autor.DtNascimento, new DateTime(1948, 9, 20));
+        }
+
+        [TestMethod]
+        public void TestListaAutores()
+        {
+            Autor autor = new Autor();
+            List<Autor> lstAutores = autor.ListaAutores();
+
+            foreach (var aut in lstAutores)
+            {
+                Assert.IsInstanceOfType(aut, typeof(Autor));
+            }
         }
 
     }
